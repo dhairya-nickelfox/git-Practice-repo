314e48f1bc7484c943c891fe521c5d5fc3dd7fbe
Dhairya_009 <dhairyabhardwaj@Nickelfoxs-MacBook-Air-3.local>
2024-12-12 11:30:38 +0530

    git practice with xcode project

---
+++ git Practice/git_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  git_PracticeApp.swift
+//  git Practice
+//
+//  Created by Dhairya Bhardwaj on 12/12/24.
+//
+
+import SwiftUI
+
+@main
+struct git_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
